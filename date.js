44adffb6cd507478556cc9614f07a979268c46ef
Makesh N R <53849867+mak-simus@users.noreply.github.com>
2023-06-07 01:39:43 +0530

    Add files via upload

---
+++ date.js
@@ -0,0 +1,42 @@
+
+
+exports.getDate = function(){
+
+
+
+
+    let today = new Date();
+
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month:"long"
+    };
+  
+    return today.toLocaleDateString("ja-JP", options);
+
+   
+
+
+}
+
+exports.getDay = function(){
+
+
+
+
+    let today = new Date();
+
+    let options = {
+        weekday: "long",
+        
+    };
+  
+    return today.toLocaleDateString("ja-JP", options);
+
+    
+
+}
+
+
+
